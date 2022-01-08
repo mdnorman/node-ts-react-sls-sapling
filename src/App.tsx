edb4e62edd50ae24c217ef1e78c2630240d37095
Michael D. Norman <michael.norman@denormans.com>
2022-01-08 15:14:22 -0600

    chore: fixed compile errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
 import * as React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { Main } from './pages/Main';
-import { BadRoute } from './pages/BadRoute';
+import AppRoutes from './components/common/routes/AppRoutes';
 
-export class App extends React.Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route path="*" component={BadRoute} />
-      </Switch>
-    );
-  }
-}
+const App = () => <AppRoutes />;
+export default App;
